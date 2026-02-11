18aef8265251fafc6dd0936fd56689b22c8e851b
张洋 <18153640105@189.cn>
2026-02-11 14:53:01 +0800

    Change

---
+++ demo-study/src/main/java/com/java/ClassA.java
@@ -0,0 +1,13 @@
+//package com.java;
+//
+//import org.springframework.stereotype.Component;
+//import com.java.ClassI;
+//
+//@Component
+//public class ClassA implements ClassI {
+//
+//    public String send(Integer i) {
+//        System.out.println("ClassA");
+//        return null;
+//    }
+//}
--- demo-study/src/main/java/com/java/ClassI.java
+++ demo-study/src/main/java/com/java/ClassI.java
@@ -1,9 +1,8 @@
-package com.base.java;
+package com.java;
 
 
 public interface ClassI {
     
     public String send(Integer i);
-    
-    
+
 }
